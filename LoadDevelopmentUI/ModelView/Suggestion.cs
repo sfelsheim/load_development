0583517fc5988375a0100ea3cb978fa6829b6454
sfelsheim <steve.felsheim@gmail.com>
2020-04-13 15:47:01 -0500

    Fix issue #1

---
+++ LoadDevelopmentUI/ModelView/Suggestion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace LoadDevelopmentUI.ModelView
+{
+    public class Suggestion : INotifyPropertyChanged
+    {
+        private bool isChecked = false;
+        public string Display { get; set; }
+        public string Detail { get; set; }
+        public bool ShowSelect { get; } = true;
+        public bool IsChecked
+        {
+            get { return isChecked; }
+            set
+            {
+                if (value != isChecked)
+                {
+                    isChecked = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsChecked"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
